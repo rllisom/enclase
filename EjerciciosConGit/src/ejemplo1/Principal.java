47623c6e5076e8bb1a7ed0c563243f4b9e641351
Raúl Llinares <raullisom@gmail.com>
2025-02-03 00:33:08 +0100

    Main cambiado

--- EjerciciosConGit/src/ejemplo1/Principal.java
+++ EjerciciosConGit/src/ejemplo1/Principal.java
@@ -4,8 +4,12 @@ public class Principal {
 
     public static void main(String[] args) {
         int num1 = 1,num2 = 2;
+        int res;
         System.out.println("Hola mundo");
         System.out.println("La suma de " + num1 + " + " + num2 + " es " + (num1 + num2));
 
+
+        res = (num1 + num2)/3;
+        
     }
 }
