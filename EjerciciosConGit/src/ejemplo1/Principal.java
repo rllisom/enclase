964bd8c73de19ce73b7f5f1e7654596dbbe16aab
Raúl Llinares <raullisom@gmail.com>
2025-02-03 00:23:47 +0100

    Cálculo de la suma de dos números

--- EjerciciosConGit/src/ejemplo1/Principal.java
+++ EjerciciosConGit/src/ejemplo1/Principal.java
@@ -3,8 +3,9 @@ package ejemplo1;
 public class Principal {
 
     public static void main(String[] args) {
-        
+        int num1 = 1,num2 = 2;
         System.out.println("Hola mundo");
-    
+        System.out.println("La suma de " + num1 + " + " + num2 + " es " + (num1 + num2));
+
     }
 }
