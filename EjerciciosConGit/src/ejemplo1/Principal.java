9ca236d0b5718617adb5ba1f3b16c657c3e9cf53
Raúl Llinares <raullisom@gmail.com>
2025-02-03 00:31:09 +0100

    Rama act

--- EjerciciosConGit/src/ejemplo1/Principal.java
+++ EjerciciosConGit/src/ejemplo1/Principal.java
@@ -7,5 +7,6 @@ public class Principal {
         System.out.println("Hola mundo");
         System.out.println("La suma de " + num1 + " + " + num2 + " es " + (num1 + num2));
 
+        System.out.println("La multiplicación de " + num1 + " * " + num2 + " es  " + (num1 * num2));
     }
 }
